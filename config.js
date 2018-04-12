84270bc5f7deeca9fab82ec6d20c9a82399a1756
ghostlwj <ghostlwj@gmail.com>
2018-04-12 17:56:17 +0800

    Push example file config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  fb: {
+    account: '<YOUR FB ACCOUNT>',
+    password: '<YOUR FB PASSWORD>'
+  },
+  mysql: {
+    database: '<YOUR DB DATABASE>',
+    username: '<YOUR DB USER NAME>',
+    password: '<YOUR DB PASSWORD>',
+    host: '<YOUR DB HOST>'
+  }
+};
